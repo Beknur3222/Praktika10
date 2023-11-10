2df92d83aaf7749d9e516d38b208a33023276d10
Бекнур Саттар <www.asemush@gmail.com>
2023-11-10 12:31:42 +0600

    Добавьте файлы проекта.

---
+++ Dop10/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dop10
+{
+    interface IStudent
+    {
+        string Name { get; set; }
+        string FullName { get; set; }
+        int[] Grades { get; set; }
+
+        string GetName();
+        string GetFullName();
+        double GetAvgGrade();
+    }
+
+    class Student : IStudent
+    {
+        public string Name { get; set; }
+        public string FullName { get; set; }
+        public int[] Grades { get; set; }
+
+        public Student(string name, string fullName, int[] grades)
+        {
+            Name = name;
+            FullName = fullName;
+            Grades = grades;
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public string GetFullName()
+        {
+            return FullName;
+        }
+
+        public double GetAvgGrade()
+        {
+            if (Grades == null || Grades.Length == 0)
+            {
+                return 0.0;
+            }
+
+            int sum = 0;
+            foreach (var grade in Grades)
+            {
+                sum += grade;
+            }
+
+            return (double)sum / Grades.Length;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            int[] grades = { 90, 85, 92, 88, 95 };
+            IStudent student = new Student("Beknur", "Beknur Sattar", grades);
+
+            Console.WriteLine($"Name: {student.GetName()}");
+            Console.WriteLine($"Full Name: {student.GetFullName()}");
+            Console.WriteLine($"Average Grade: {student.GetAvgGrade()}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Praktika10/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktika10
+{
+    interface IPart
+    {
+        void Build();
+    }
+
+    class Basement : IPart
+    {
+        public void Build()
+        {
+            Console.WriteLine("Фундамент построен");
+        }
+    }
+
+    class Wall : IPart
+    {
+        public void Build()
+        {
+            Console.WriteLine("Стена построена");
+        }
+    }
+
+    class Door : IPart
+    {
+        public void Build()
+        {
+            Console.WriteLine("Дверь установлена");
+        }
+    }
+
+    class Window : IPart
+    {
+        public void Build()
+        {
+            Console.WriteLine("Окно установлено");
+        }
+    }
+
+    class Roof : IPart
+    {
+        public void Build()
+        {
+            Console.WriteLine("Крыша установлена");
+        }
+    }
+
+    interface IWorker
+    {
+        void Work(IPart part);
+    }
+
+    class Worker : IWorker
+    {
+        public void Work(IPart part)
+        {
+            part.Build();
+        }
+    }
+
+    interface ITeamLeader
+    {
+        void Report(List<IPart> parts);
+    }
+
+    class TeamLeader : ITeamLeader
+    {
+        public void Report(List<IPart> parts)
+        {
+            Console.WriteLine("Отчет бригадира:");
+            foreach (var part in parts)
+            {
+                part.Build();
+            }
+        }
+    }
+
+    class Team
+    {
+        private List<IPart> parts;
+
+        public Team()
+        {
+            parts = new List<IPart>();
+        }
+
+        public void AddPart(IPart part)
+        {
+            parts.Add(part);
+        }
+
+        public void StartBuilding()
+        {
+            foreach (var part in parts)
+            {
+                IWorker worker = new Worker();
+                worker.Work(part);
+            }
+
+            ITeamLeader teamLeader = new TeamLeader();
+            teamLeader.Report(parts);
+
+            Console.WriteLine("Строительство дома завершено.");
+        }
+    }
+
+    class House
+    {
+        static void Main()
+        {
+            Team team = new Team();
+
+            team.AddPart(new Basement());
+            team.AddPart(new Wall());
+            team.AddPart(new Wall());
+            team.AddPart(new Wall());
+            team.AddPart(new Wall());
+            team.AddPart(new Door());
+            team.AddPart(new Window());
+            team.AddPart(new Window());
+            team.AddPart(new Window());
+            team.AddPart(new Window());
+            team.AddPart(new Roof());
+
+            team.StartBuilding();
+            Console.ReadKey();
+        }
+    }
+
+}
